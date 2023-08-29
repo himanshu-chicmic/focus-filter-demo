06512417c8550b8e5a0a95a3875ffa5adf49e41a
Himanshu Goyal <himanshu.goyal@chicmic.co.in>
2023-08-29 14:13:20 +0530

    init(project-setup): add project files

---
+++ FocusFilter/FocusFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFilterApp.swift
+//  FocusFilter
+//
+//  Created by Nitin on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusFilterIntent/FocusFilterIntentExtension.swift
@@ -0,0 +1,12 @@
+//
+//  FocusFilterIntentExtension.swift
+//  FocusFilterIntent
+//
+//  Created by Nitin on 8/29/23.
+//
+
+import AppIntents
+
+@main
+struct FocusFilterIntentExtension: AppIntentsExtension {
+}
